6a31ba5be74981e2c0d660861047d2d73d97a2ea
Durgashankar Pal <durgashankarpal14285@gmail.com>
2022-09-11 19:45:14 +0530

    Done

--- src/Componetns/About/About.jsx
+++ src/Componetns/About/About.jsx
@@ -9,40 +9,40 @@ import "aos/dist/aos.css"
 import { useEffect } from 'react'
 
 const About = () => {
-  useEffect(()=>{
-    Aos.init({duration:1000})
-  },[])
+  // useEffect(()=>{
+  //   Aos.init({duration:1000})
+  // },[])
   return (
     <section id='about'>
-      <h5 data-aos="fade-up">Get to Know</h5>
-      <h2 data-aos="fade-up">About Me</h2>
+      <h5 >Get to Know</h5>
+      <h2 >About Me</h2>
       <div className='container about_container'>
-      <div data-aos="fade-in" data-aos-duration="2000" className="about_me">
+      <div  data-aos-duration="2000" className="about_me">
         <div className="about_me_image">
           <img src={ME} alt="" />
         </div>
       </div>
       <div className="about_content">
         <div className="about_cards">
-          <article data-aos="fade-right" className='about_card'>
+          <article  className='about_card'>
             <FaAward className='about_icon'/>
             <h5>DSA</h5>
             <small>250+ DSA Problems</small>
           </article>
-          <article data-aos="fade-up" className='about_card'>
+          <article  className='about_card'>
             <VscFolderLibrary className='about_icon'/>
             <h5>Projects</h5>
             <small>6+ projects</small>
           </article>
-          <article data-aos="fade-left" className='about_card'>
+          <article className='about_card'>
             <VscFolderLibrary className='about_icon'/>
             <h5>Graduation</h5>
             <small>Bsc (Physical Science)</small>
           </article>
         </div>
 
-        <p data-aos='fade-left'>I am a Full Stack developer with experience in building websites and web applications. I specialize MERN stack, along with an expertise in JavaScript, HTML, CSS, and React. Outside of coding, my interest lies traveling..</p>
-        <a data-aos="fade-left" href="#contact" className='btn btn_primary'>Let's Talk</a>
+        <p >I am a Full Stack developer with experience in building websites and web applications. I specialize MERN stack, along with an expertise in JavaScript, HTML, CSS, and React. Outside of coding, my interest lies traveling..</p>
+        <a  href="#contact" className='btn btn_primary'>Let's Talk</a>
       </div>
       </div>
     </section>
